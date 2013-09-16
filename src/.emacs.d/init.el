d0e84bf7824ef6086687bc0316953fc7a54533a5
Marten Lienen <marten.lienen@gmail.com>
2013-09-16 11:19:08 +0200

    Install clojure stuff

--- src/.emacs.d/init.el
+++ src/.emacs.d/init.el
@@ -4,17 +4,28 @@
 
 (defvar my-packages
   '(clojure-mode
-    color-theme-solarized))
+    clojure-test-mode
+    nrepl
+    paredit
+    rainbow-delimiters
+    color-theme-solarized
+    markdown-mode))
 
 (defun ensure-package-installed (package)
   (when (not (package-installed-p package))
     (package-install package)))
+
+(when (null package-archive-contents)
+	(package-refresh-contents))
 
 (mapc 'ensure-package-installed my-packages)
 
 ;; UI
 
 (load-theme 'solarized-dark t)
+
+; Color nested parens rainbow-like
+(global-rainbow-delimiters-mode)
 
 ; Disable menu bar
 (menu-bar-mode -1)
@@ -25,3 +36,16 @@
 ; Disable backups and autosaves
 (setq backup-inhibited t)
 (setq auto-save-default nil)
+
+
+;; Editing
+
+; Automatically indent
+(electric-indent-mode 1)
+
+(autoload 'enable-paredit-mode "paredit" "Slurp those parens" t)
+(defvar my-lisp-mode-hooks
+  '(emacs-lisp-mode-hook
+    clojure-mode-hook
+    nrepl-mode-hook))
+(mapc (lambda (hook) (add-hook hook #'enable-paredit-mode)) my-lisp-mode-hooks)
