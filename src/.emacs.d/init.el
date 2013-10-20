bca91979c531c28fd5951871004b47482a518e92
Marten Lienen <marten.lienen@gmail.com>
2013-10-20 15:14:40 +0200

    Autosave tex files

--- src/.emacs.d/init.el
+++ src/.emacs.d/init.el
@@ -148,7 +148,8 @@
 (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 (setq TeX-electric-sub-and-superscript t)
-(setq TeX-PDF-mode t)
+(setq TeX-PDF-mode t) ; Produce PDFs
+(setq TeX-save-query nil) ; Autosave files before compilation
 (add-to-list 'auto-mode-alist '("\\.tex\\'" . LaTeX-mode))
 
 
