0eed4137bb1459b24e63b4f1b20ffe4721c261b3
Marten Lienen <marten.lienen@gmail.com>
2013-10-23 22:27:13 +0200

    Change yas-expand key

--- src/.emacs.d/init.el
+++ src/.emacs.d/init.el
@@ -130,6 +130,9 @@
 ;; yasnippet
 (require 'yasnippet)
 (setq yas-fallback-behavior 'call-other-command)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+(define-key yas-minor-mode-map (kbd ";") 'yas-expand)
+; Don't append newlines to snippet files
 (add-hook 'snippet-mode (lambda () (setq require-final-newline nil)))
 (yas/load-directory "~/.emacs.d/snippets")
 (yas-global-mode t)
