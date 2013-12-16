8e2a69c0488cca124af1c3366c7badbd66bc2bdd
Marten Lienen <marten.lienen@gmail.com>
2013-12-16 13:33:29 +0100

    Disable emacs-lisp checker

--- src/.emacs.d/configs/flycheck.el
+++ src/.emacs.d/configs/flycheck.el
@@ -1,4 +1,4 @@
 (require 'flycheck)
 
 (add-hook 'after-init-hook 'global-flycheck-mode)
-(setq-default flycheck-disabled-checkers (append flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+(setq-default flycheck-disabled-checkers (append flycheck-disabled-checkers '(emacs-lisp emacs-lisp-checkdoc)))
