1032e99b94b98b3ee8c6cec184910d0563fd518c
Marten Lienen <marten.lienen@gmail.com>
2014-01-04 19:08:43 +0100

    Indent CSS with 2 spaces

--- src/.emacs.d/globals.el
+++ src/.emacs.d/globals.el
@@ -33,9 +33,10 @@
 (setq gc-cons-threshold 20000000)
 
 ;; Indent with 2 spaces
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
-(setq js-indent-level 2)
+(setq-default indent-tabs-mode nil
+              tab-width 2
+              js-indent-level 2
+              css-indent-offset 2)
 
 ;; Disable backups and autosaves
 (setq backup-inhibited t)
