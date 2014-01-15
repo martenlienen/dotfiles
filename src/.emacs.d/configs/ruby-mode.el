9247f219b5d47d7ee32ede32bc79ef1bf2e5db2c
Marten Lienen <marten.lienen@gmail.com>
2014-01-15 22:34:28 +0100

    Use ruby-mode in gemspec and Rakefile

--- src/.emacs.d/configs/ruby-mode.el
+++ src/.emacs.d/configs/ruby-mode.el
@@ -6,3 +6,5 @@
 (add-hook 'ruby-mode-hook 'robe-mode)
 
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '(".gemspec\\'" . ruby-mode))
