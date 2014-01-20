f6926a8bf9ce16bd228a9e595a7c5d01722d80f1
Marten Lienen <marten.lienen@gmail.com>
2014-01-20 16:05:07 +0100

    Use ruby-electric-mode

--- src/.emacs.d/configs/ruby-mode.el
+++ src/.emacs.d/configs/ruby-mode.el
@@ -5,6 +5,7 @@
       ruby-insert-encoding-magic-comment nil)
 
 (add-hook 'ruby-mode-hook 'robe-mode)
+(add-hook 'ruby-mode-hook 'ruby-electric-mode)
 
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
