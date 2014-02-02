5ce9dbc6da08fb9e4055b0be90b8c9b299a3363c
Marten Lienen <marten.lienen@gmail.com>
2014-02-02 13:28:22 +0100

    Delete auto-complete mode

--- src/.emacs.d/configs/LaTeX.el
+++ src/.emacs.d/configs/LaTeX.el
@@ -3,9 +3,7 @@
 
 (require 'latex)
 (require 'tex-site)
-(require 'ac-math)
 
-(add-hook 'LaTeX-mode-hook 'auto-complete-mode)
 (add-hook 'LaTeX-mode-hook 'ax-latex-mode-setup)
 (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
@@ -16,9 +14,3 @@
       TeX-view-program-selection '((output-pdf "Okular")))
 
 (add-to-list 'auto-mode-alist '("\\.tex\\'" . LaTeX-mode))
-
-(setq ac-sources
-      (append '(ac-source-math-unicode ac-source-math-latex ac-source-latex-commands)
-              ac-sources))
-
-(add-to-list 'ac-modes 'LaTeX-mode)
