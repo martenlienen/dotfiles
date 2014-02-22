ad94c189fbcbc079b5feabfdd729f5070206dbbc
Marten Lienen <marten.lienen@gmail.com>
2014-02-22 03:19:47 +0100

    Add xmonad configuration

---
+++ src/.xmonad/xmonad.hs
@@ -0,0 +1,9 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmonad $ defaultConfig
