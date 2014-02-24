3bb742f41b80203e335179693690391ce56eec40
Marten Lienen <marten.lienen@gmail.com>
2014-02-24 12:46:39 +0100

    Configure xmonad output

--- src/.xmonad/xmonad.hs
+++ src/.xmonad/xmonad.hs
@@ -5,8 +5,22 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
-main = xmonad =<< xmobar myConfig
+main = xmonad =<< bar myConfig
+  where
+    bar = statusBar "xmobar" myPP toggleXmobarKey
 
 myConfig = defaultConfig {
         modMask = mod4Mask
     }
+
+myPP = defaultPP { ppCurrent = xmobarColor "white" "" . wrap "<" ">",
+                   ppHidden = xmobarColor "gray" "",
+                   ppHiddenNoWindows = xmobarColor "gray" "",
+                   ppUrgent = xmobarColor "red" "",
+                   ppLayout = xmobarColor "blue" "",
+                   ppTitle = xmobarColor "green" "" . shorten 80,
+                   ppSep = xmobarColor "purple" "" " # "
+                 }
+
+toggleXmobarKey :: XConfig t -> (KeyMask, KeySym)
+toggleXmobarKey XConfig{modMask = modM} = (modM, xK_b)
