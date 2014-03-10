06322f1f0a282b9f4334b51963b34a807e4b95bb
Marten Lienen <marten.lienen@gmail.com>
2014-03-10 14:27:46 +0100

    Use ruby-mode in Vagrantfiles

--- src/.emacs.d/configs/ruby-mode.el
+++ src/.emacs.d/configs/ruby-mode.el
@@ -10,5 +10,6 @@
 
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '(".gemspec\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '(".json_builder\\'" . ruby-mode))
