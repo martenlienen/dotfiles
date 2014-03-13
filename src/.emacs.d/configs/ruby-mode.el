586ecdc6679b35ab1b6d2f76f8cd415b867a7fa6
Marten Lienen <marten.lienen@gmail.com>
2014-03-13 23:57:12 +0100

    Use ruby-mode in Capfiles

--- src/.emacs.d/configs/ruby-mode.el
+++ src/.emacs.d/configs/ruby-mode.el
@@ -10,6 +10,7 @@
 
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Berksfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '(".gemspec\\'" . ruby-mode))
