101dd3c5062e0bc8918a1af68e36957929119777
Marten Lienen <marten.lienen@gmail.com>
2014-03-24 10:19:03 +0100

    Set default value of js indent to 2

--- src/.emacs.d/configs/js2-mode.el
+++ src/.emacs.d/configs/js2-mode.el
@@ -6,7 +6,7 @@
 
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
-(setf js2-basic-offset 2
-      js2-highlight-level 3)
+(setq-default js2-basic-offset 2)
+(setf js2-highlight-level 3)
 
 (js2r-add-keybindings-with-prefix "C-c r")
