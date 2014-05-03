e38ec17e68879fe123591133243bc44164e6e2f9
Marten Lienen <marten.lienen@gmail.com>
2014-05-03 19:15:26 +0200

    Enable volume keys

--- src/.xmonad/xmonad.hs
+++ src/.xmonad/xmonad.hs
@@ -2,15 +2,19 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig(additionalKeysP)
 import System.IO
 
-main = xmonad =<< bar myConfig
+main = xmonad =<< bar config
   where
+    config = myConfig `additionalKeysP` myKeys
     bar = statusBar "xmobar" myPP toggleXmobarKey
 
 myConfig = defaultConfig { modMask = mod4Mask
                          }
+
+myKeys = [ ("<XF86AudioRaiseVolume>", spawn "amixer -c 0 -- sset Master '2.00dB+'" )
+         , ("<XF86AudioLowerVolume>", spawn "amixer -c 0 -- sset Master '1.00dB-'" ) ]
 
 myPP = defaultPP { ppCurrent = xmobarColor "white" "" . wrap "<" ">",
                    ppHidden = xmobarColor "#3E5247" "",
