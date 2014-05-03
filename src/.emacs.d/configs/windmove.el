1cecac024dbad3d8a61ba62c73dc35ae5c5f0031
Marten Lienen <marten.lienen@gmail.com>
2014-05-03 13:35:06 +0200

    Replace windmove with ace-window

--- src/.emacs.d/configs/windmove.el
+++
@@ -1,1 +0,0 @@
-(windmove-default-keybindings)
--- src/.emacs.d/lisp/bindings.el
+++ src/.emacs.d/lisp/bindings.el
@@ -19,6 +19,7 @@
  ("C-x f" 'find-file)
  ("M-s" 'ace-jump-word-mode)
  ("M-S" 'ace-jump-char-mode)
+ ("M-p" 'ace-window)
  ("<M-return>" 'magit-status)
  ("M-x" 'smex)
  ("C-z" popwin:keymap)
