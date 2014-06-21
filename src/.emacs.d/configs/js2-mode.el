41b1bc7af118d53b7081f2aa72097c73570a2400
Marten Lienen <marten.lienen@gmail.com>
2014-06-21 20:28:38 +0200

    Add js2-mode snippets

--- src/.emacs.d/configs/js2-mode.el
+++ src/.emacs.d/configs/js2-mode.el
@@ -7,7 +7,8 @@
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
 (setq-default js2-basic-offset 2)
-(setf js2-highlight-level 3)
+(setf js2-highlight-level 3
+      js2-include-node-externs t)
 
 (js2r-add-keybindings-with-prefix "C-c r")
 
