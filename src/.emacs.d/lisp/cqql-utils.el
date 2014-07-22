69942f94520554b2cdb5de1648e25a8d6ae55b8f
Marten Lienen <marten.lienen@gmail.com>
2014-07-23 01:24:21 +0200

    Do not require js2-mode

--- src/.emacs.d/lisp/cqql-utils.el
+++ src/.emacs.d/lisp/cqql-utils.el
@@ -58,6 +58,11 @@
   `(eval-after-load ,feature
      '(progn ,@body)))
 
+(defun cqql/add-auto-mode (mode &rest patterns)
+  "Add entries to `auto-mode-alist' to use `MODE' for all given file `PATTERNS'."
+  (dolist (pattern patterns)
+    (add-to-list 'auto-mode-alist (cons pattern mode))))
+
 (defmacro cqql/define-keys (keymap &rest bindings)
   `(progn
      ,@(-map
