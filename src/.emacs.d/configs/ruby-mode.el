234acb7b7394698bc1a22798290800437827df0b
Marten Lienen <marten.lienen@gmail.com>
2014-07-27 17:15:32 +0200

    Use inf-ruby

--- src/.emacs.d/configs/ruby-mode.el
+++ src/.emacs.d/configs/ruby-mode.el
@@ -2,7 +2,7 @@
   (setf ruby-insert-encoding-magic-comment nil)
 
   (add-hook 'ruby-mode-hook 'robe-mode)
-  (add-hook 'ruby-mode-hook 'ruby-electric-mode)
+  (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
   (add-hook 'ruby-mode-hook 'yard-mode)
   (add-hook 'ruby-mode-hook 'eldoc-mode)
   (add-hook 'ruby-mode-hook 'subword-mode))
