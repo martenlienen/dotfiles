ebc5e8af3af026ca6ef45f15db620a604b1a4c14
Marten Lienen <marten.lienen@gmail.com>
2014-08-14 12:26:59 +0200

    Disable horizontal scroll bars

--- src/.emacs.d/lisp/globals.el
+++ src/.emacs.d/lisp/globals.el
@@ -3,10 +3,9 @@
 
 ;; Disable visual stuff
 (menu-bar-mode -1)
-(when (fboundp 'tool-bar-mode)
-  (tool-bar-mode -1))
-(when (fboundp 'scroll-bar-mode)
-  (scroll-bar-mode -1))
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(horizontal-scroll-bar-mode -1)
 
 (setf ring-bell-function 'ignore)
 
