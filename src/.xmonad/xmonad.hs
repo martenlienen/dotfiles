b8afe99243609bb39bef511a1c8d7832f55448f2
Marten Lienen <marten.lienen@gmail.com>
2014-11-26 22:17:22 +0100

    Take screenshots with maim

--- src/.xmonad/xmonad.hs
+++ src/.xmonad/xmonad.hs
@@ -17,13 +17,17 @@ myConfig = defaultConfig { modMask = mod4Mask, startupHook = setWMName "LG3D" }
 
 dbusMsg msg = "dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 \"org.freedesktop.login1.Manager." ++ msg ++ "\" boolean:true > /home/cqql/log"
 
+maim opts = "maim " ++ opts ++ " ~/$(date +%F-%T).png"
+
 myKeys = [ ("<XF86AudioRaiseVolume>", spawn "amixer -c 0 -- sset Master '2.00dB+'")
          , ("<XF86AudioLowerVolume>", spawn "amixer -c 0 -- sset Master '1.00dB-'")
          , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 10 -time 0")
          , ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10 -time 0")
          , ("M-<F10>", spawn (dbusMsg "PowerOff"))
          , ("M-<F11>", spawn (dbusMsg "Reboot"))
-         , ("M-<F12>", spawn (dbusMsg "Suspend")) ]
+         , ("M-<F12>", spawn (dbusMsg "Suspend"))
+         , ("M-s", spawn (maim ""))
+         , ("M-S-s", spawn (maim "-s")) ]
 
 myPP = defaultPP { ppCurrent = xmobarColor "white" "" . wrap "<" ">",
                    ppHidden = xmobarColor "#3E5247" "",
