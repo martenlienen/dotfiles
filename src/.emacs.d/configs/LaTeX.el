9ce4fcea0290b1fbf5c9a150690f231e4ba69852
Marten Lienen <marten.lienen@gmail.com>
2014-11-29 21:21:48 +0100

    Use minted for code in LaTeX

--- src/.emacs.d/configs/LaTeX.el
+++ src/.emacs.d/configs/LaTeX.el
@@ -10,8 +10,11 @@
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 (add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
 
-(setq TeX-electric-sub-and-superscript t
-      TeX-save-query nil
-      TeX-view-program-selection '((output-pdf "Okular")))
+(add-hook 'LaTeX-mode-hook (lambda ()
+                             (setq TeX-electric-sub-and-superscript t
+                                   TeX-save-query nil
+                                   TeX-view-program-selection '((output-pdf "Okular"))
+                                   ;; Otherwise minted can't find pygments
+                                   TeX-command-extra-options "-shell-escape")))
 
 (add-to-list 'auto-mode-alist '("\\.tex\\'" . LaTeX-mode))
