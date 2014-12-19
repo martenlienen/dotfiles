8e234decfbfabcca826e1aa16fcb45cd42cbca75
Marten Lienen <marten.lienen@gmail.com>
2014-12-19 16:21:14 +0100

    Use flycheck for elisp

--- src/.emacs.d/configs/emacs-lisp-mode.el
+++ src/.emacs.d/configs/emacs-lisp-mode.el
@@ -1,4 +1,5 @@
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook 'flycheck-mode)
 (add-hook 'emacs-lisp-mode-hook 'smartparens-strict-mode)
 
 (add-to-list 'auto-mode-alist '("Cask\\'" . emacs-lisp-mode))
