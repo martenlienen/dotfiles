d2d161fd20c4903d21842437c70674e5efb08b13
Marten Lienen <marten.lienen@gmail.com>
2015-10-21 21:44:58 +0200

    Generate a structure of exercise (part) headers

--- src/.emacs.d/lisp/cqql.el
+++ src/.emacs.d/lisp/cqql.el
@@ -2,24 +2,24 @@
 
 ;;; Commentary:
 
-;;; Code:    
+;;; Code:
 
 (require 'dash)
 
 (defun cqql-go-to-beginning-of-line-dwim ()
+  "Toggle point between beginning of line and first non-whitespace character."
   (interactive)
-  "Toggle point between beginning of line and first non-whitespace character"
   (let ((prev-pos (point)))
     (back-to-indentation)
     (when (= prev-pos (point))
       (move-beginning-of-line nil))))
 
 (defun cqql-duplicate-text (times)
-  (interactive "p")
-  "Duplicate the current line or region TIMES times
+  "Duplicate the current line or region TIMES times.
 
 If the region is active, it duplicates from the start of the
 first line of the region to the end of the last."
+  (interactive "p")
   (let* ((region? (use-region-p))
          (pos (point))
          (text-start (save-excursion
@@ -43,28 +43,39 @@ first line of the region to the end of the last."
     (setf (point) new-pos)))
 
 (defun cqql-open-line ()
+  "Create a new line below and put point into it."
   (interactive)
-  "Create a new line below and put point into it"
   (move-end-of-line nil)
   (newline-and-indent))
 
 (defun cqql-open-line-above ()
+  "Create a new line above point and move point into it."
   (interactive)
-  "Create a new line above point and move point into it"
   (move-beginning-of-line nil)
   (newline)
   (forward-line -1)
   (indent-according-to-mode))
 
 (defun cqql-kill-line ()
+  "Kill the current line."
   (interactive)
-  "Kill the current line"
   (let ((pos (point)))
     (move-beginning-of-line nil)
     (kill-line 1)
     (move-end-of-line nil)
     (when (< pos (point))
       (setf (point) pos))))
+
+(defun cqql-exercise-headers (structure)
+  "Create a STRUCTURE of headers for exercise sheets."
+  (interactive "xStructure: ")
+  (cl-loop for node in structure
+           do
+           (if (listp node)
+               (cl-loop for subnode in node
+                        do (insert (format "\\subsection*{Part %s)}\n\n"
+                                           subnode)))
+             (insert (format "\\section*{Exercise %s}\n\n" node)))))
 
 (defvar cqql-no-trimming-modes '()
   "A list of modes, that should not be whitespace-trimmed.")
