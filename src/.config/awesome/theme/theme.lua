77fe1ff3de4c44a712404909ad7e5ea81b3b0e0e
Marten Lienen <marten.lienen@gmail.com>
2016-01-10 16:24:22 +0100

    Select a wallpaper randomly

--- src/.config/awesome/theme/theme.lua
+++ src/.config/awesome/theme/theme.lua
@@ -1,10 +1,18 @@
----------------------------
--- Default awesome theme --
----------------------------
+local wallpaper = "/usr/share/awesome/themes/default/background.png"
+local wallpapers = {}
+
+output = io.popen("ls " .. os.getenv("HOME") .. "/.wallpapers")
+for filename in output:lines() do
+   table.insert(wallpapers, os.getenv("HOME") .. "/.wallpapers/" .. filename)
+end
+
+if #wallpapers > 0 then
+   wallpaper = wallpapers[math.random(#wallpapers)]
+end
 
 theme = {}
-
-theme.font          = "Dejavu Sans Mono 8"
+theme.font = "Dejavu Sans Mono 8"
+theme.wallpaper = wallpaper
 
 theme.bg_normal     = "#222222"
 theme.bg_focus      = "#535d6c"
@@ -72,8 +80,6 @@ theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
-
-theme.wallpaper = "/usr/share/awesome/themes/default/background.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
