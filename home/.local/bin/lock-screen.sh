892525fc8b8df1b8184e85e5f045347062d07c26
Marten Lienen <marten.lienen@gmail.com>
2018-04-29 13:25:31 +0200

    Ignore empty passwords

--- home/.local/bin/lock-screen.sh
+++ home/.local/bin/lock-screen.sh
@@ -5,6 +5,6 @@
 tmp=$(mktemp --dry-run --tmpdir XXX.png)
 
 maim --hidecursor | convert - -scale 7.5% -scale 1333.33% "$tmp"
-i3lock --no-unlock-indicator --image="$tmp"
+i3lock --no-unlock-indicator --image="$tmp" --ignore-empty-password
 
 rm "$tmp"
