8683de61afe297daee57578f5dc67976de1022ce
Marten Lienen <marten.lienen@gmail.com>
2019-08-18 22:16:20 +0200

    Make flycheck happy

--- home/.emacs.d/lisp/cqql-deadgrep.el
+++ home/.emacs.d/lisp/cqql-deadgrep.el
@@ -1,4 +1,14 @@
+;;; cqql-deadgrep.el --- Shortcuts for deadgrep
+
+;;; Commentary:
+
+;;; Code:
+
 (defun cqql-deadgrep-search-term ()
+  "Change the search term in deadgrep."
   (interactive)
   ;; Ignores the button so we can just call it
   (deadgrep--search-term nil))
+
+(provide 'cqql-deadgrep)
+;;; cqql-deadgrep.el ends here
