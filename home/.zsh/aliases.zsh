8f5914f39020c9d7d4fd9903f4c4e603a866a5b9
Marten Lienen <marten.lienen@gmail.com>
2020-04-14 17:43:21 +0200

    Compact aliases

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -6,43 +6,28 @@ alias spu="sudo pacman -Syu"
 
 alias l="ls -lh"
 
-# Aliases
 alias g="git"
-
 alias gst="git status"
-
 alias ga="git add"
-
 alias gb="git branch"
-
 alias gco="git checkout"
-
 alias gc="git commit"
 alias gca="git commit --amend"
-
 alias gd="git diff"
 alias gdc="git diff --cached"
-
 alias gl="git log"
-
 alias grs="git reset"
 alias grsp="git reset --soft HEAD^"
 alias grsh="git reset HEAD"
-
 alias gcl="git clone"
-
 alias grm="git remote"
-
 alias gf="git fetch"
 alias gP="git push"
 alias gp="git pull"
-
 alias gm="git merge"
-
 alias gcp="git cherry-pick"
 alias gcpc="git cherry-pick --continue"
 alias gcpa="git cherry-pick --abort"
-
 alias grb="git rebase"
 alias grbi="git rebase --interactive"
 alias grbc="git rebase --continue"
