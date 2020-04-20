294960df9701eb7508bcc2943a9dcd0a80c87552
Marten Lienen <marten.lienen@gmail.com>
2020-04-20 18:46:25 +0200

    Automatically make an RNG object available

--- home/.ipython/profile_default/ipython_config.py
+++ home/.ipython/profile_default/ipython_config.py
@@ -1,20 +1,30 @@
-def import_if_available(module, alias=None):
+import textwrap
+
+
+def import_if_available(module, alias=None, setup=None):
     if alias:
         import_ = "{} as {}".format(module, alias)
     else:
         import_ = module
 
+    if setup is None:
+        setup = ""
+    else:
+        setup = textwrap.indent(setup, "    ")
+
     return """
 try:
     import {}
+{}
 except ImportError:
     pass
-""".format(import_).strip()
+""".format(import_, setup).strip()
+
 
 c = get_config()
 c.InteractiveShellApp.exec_lines = [
-    import_if_available("numpy", "np"),
+    import_if_available("numpy", "np", setup="rng = np.random.default_rng()"),
     import_if_available("pandas", "pd"),
     import_if_available("matplotlib.pyplot", "pp"),
-    import_if_available("torch")
+    import_if_available("torch"),
 ]
