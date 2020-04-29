62c53e62953724304c65e1f9aac3a94f7c905024
Marten Lienen <marten.lienen@gmail.com>
2020-04-29 18:18:46 +0200

    Insert a label prefix intelligently

--- home/.emacs.d/snippets/latex-mode/.yas-setup.el
+++ home/.emacs.d/snippets/latex-mode/.yas-setup.el
@@ -6,3 +6,9 @@
 
 (defun isnip-latex-to-label (text)
   (s-dashed-words text))
+
+(defun isnip-latex-label-prefix ()
+  (let ((environment (LaTeX-current-environment)))
+    (pcase environment
+      ((or "equation" "equation*" "align" "align*") "eq:")
+      ((or "figure") "fig:"))))
