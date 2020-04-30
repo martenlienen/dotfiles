9852cc8658984498d48f989454fa0397b2851bbb
Marten Lienen <marten.lienen@gmail.com>
2020-04-30 17:52:49 +0200

    Make the label prefix sensitive to macros

--- home/.emacs.d/snippets/latex-mode/.yas-setup.el
+++ home/.emacs.d/snippets/latex-mode/.yas-setup.el
@@ -7,8 +7,20 @@
 (defun isnip-latex-to-label (text)
   (s-dashed-words text))
 
+(cl-defun isnip-latex-last-macro (&optional (n 1))
+  "Find the name of the N-th last macro."
+  (save-excursion
+    (cl-loop repeat n do (search-backward "\\"))
+    (forward-char)
+    (word-at-point)))
+
 (defun isnip-latex-label-prefix ()
-  (let ((environment (LaTeX-current-environment)))
-    (pcase environment
-      ((or "equation" "equation*" "align" "align*") "eq:")
-      ((or "figure") "fig:"))))
+  (or
+   (let ((environment (LaTeX-current-environment)))
+     (pcase environment
+       ((or "equation" "equation*" "align" "align*") "eq:")
+       ((or "figure") "fig:")))
+   (let ((macro (isnip-latex-last-macro 2)))
+     (message macro)
+     (pcase macro
+       ((or "section" "subsection" "subsubsection") "sec:")))))
