2ae318e870c7c3d4314ac4ee83db5664817fc494
Marten Lienen <marten.lienen@gmail.com>
2020-11-05 01:14:10 +0100

    Define the import level on the top level

--- home/.julia/config/startup.jl
+++ home/.julia/config/startup.jl
@@ -1,24 +1,22 @@
-try
-    using OhMyREPL
-catch e
-    @warn(e)
+macro try_import(pkg)
+    quote
+        try
+            using $pkg
+        catch e
+            @warn(e)
+        end
+    end
 end
 
-try
-    using Revise
-catch e
-    @warn(e)
-end
+@try_import OhMyREPL
+@try_import Revise
+@try_import PkgTemplates
 
-try
-    using PkgTemplates
-
+if isdefined(Main, :PkgTemplates)
     project = Template(
         user = "martenlienen",
-        authors = "Marten Lienen <marten.lienen@gmail.com",
+        authors = "Marten Lienen <marten.lienen@gmail.com>",
         julia = v"1.5",
         plugins = [!CompatHelper, !TagBot]
     )
-catch e
-    @warn(e)
 end
