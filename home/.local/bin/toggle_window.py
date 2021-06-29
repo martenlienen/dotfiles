23935441a73bf2c59830e005872b7a6f12eceb05
Marten Lienen <marten.lienen@gmail.com>
2021-06-29 17:54:47 +0200

    Use the snap todoist app

--- home/.local/bin/toggle_window.py
+++ home/.local/bin/toggle_window.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
+from argparse import ArgumentParser
 import json
 import os
 import shutil
 import subprocess
 
-SPOTIFY_PATH = shutil.which("spotify")
 I3MSG_PATH = "/usr/bin/i3-msg"
 
 
@@ -26,12 +26,12 @@ class WorkspaceException(Exception):
         self.workspace = workspace
 
 
-def find_spotify_workspace():
+def find_program_workspace(window_class):
     def search(node, workspace=None):
         if node.get("type") == "workspace":
             workspace = node
 
-        if node.get("window_properties", {}).get("class") == "Spotify":
+        if node.get("window_properties", {}).get("class") == window_class:
             raise WorkspaceException(workspace)
         else:
             if "nodes" in node:
@@ -45,25 +45,28 @@ def find_spotify_workspace():
         layout_tree = i3_layout_tree()
         search(layout_tree)
 
-        raise Exception("Spotify window not found in the layout tree")
+        raise Exception(f"{window_class} window not found in the layout tree")
     except WorkspaceException as e:
         return e.workspace
 
 
-def spotify_runs():
-    pgrep = subprocess.run(["pgrep", "spotify"], stdout=subprocess.DEVNULL)
+def program_runs(program):
+    pgrep = subprocess.run(["pgrep", program], stdout=subprocess.DEVNULL)
     return pgrep.returncode == 0
 
 
-def start_spotify():
-    os.execl(SPOTIFY_PATH, SPOTIFY_PATH)
+def start_program(program):
+    path = shutil.which(program)
+    os.execl(path, path)
 
 
-def spotify_is_visible(workspace):
+def program_is_visible(workspace):
     class FoundIt(Exception):
         pass
 
-    # Check if spotify's workspace contains the focused window
+    # Check if the program's workspace contains the focused window. This could be not the
+    # case if for example the focused window is on a workspace on a different screen that
+    # is displayed concurrently.
     def search(node):
         if node.get("focused") == True:
             raise FoundIt()
@@ -83,34 +86,45 @@ def spotify_is_visible(workspace):
         return True
 
 
-def spotify_is_on_scratchpad(workspace):
+def program_is_on_scratchpad(workspace):
     # The scratchpad has a workspace name of __i3_scratch
     return "scratch" in workspace.get("name", "")
 
 
-def show_spotify():
-    subprocess.run([I3MSG_PATH, '[class="Spotify"] scratchpad show'])
+def show_program(window_class):
+    subprocess.run([I3MSG_PATH, f'[class="{window_class}"] scratchpad show'])
 
 
-def hide_spotify():
-    subprocess.run([I3MSG_PATH, '[class="Spotify"] move scratchpad'])
+def hide_program(window_class):
+    subprocess.run([I3MSG_PATH, f'[class="{window_class}"] move scratchpad'])
 
 
 def main():
-    if spotify_runs():
-        spotify_workspace = find_spotify_workspace()
+    parser = ArgumentParser(description="Toggle a window to and from the scratchpad")
+    parser.add_argument("--class", dest="window_class", help="WM_CLASS of the window")
+    parser.add_argument("program")
+    args = parser.parse_args()
 
-        if spotify_is_visible(spotify_workspace):
-            hide_spotify()
-        elif spotify_is_on_scratchpad(spotify_workspace):
-            show_spotify()
+    program = args.program
+    window_class = args.window_class
+
+    if window_class is None:
+        window_class = program.title()
+
+    if program_runs(program):
+        workspace = find_program_workspace(window_class)
+
+        if program_is_visible(workspace):
+            hide_program(window_class)
+        elif program_is_on_scratchpad(workspace):
+            show_program(window_class)
         else:
-            # Spotify is visible on a workspace other than the active one so we
-            # move it here via the scratchpad
-            hide_spotify()
-            show_spotify()
+            # The program is visible on a workspace other than the active one so we move
+            # it here via the scratchpad
+            hide_program(window_class)
+            show_program(window_class)
     else:
-        start_spotify()
+        start_program(program)
 
 
 if __name__ == "__main__":
