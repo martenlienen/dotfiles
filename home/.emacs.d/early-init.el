f72844e8fd45e0ef58869f9079adc7017d0aef5e
Marten Lienen <marten.lienen@gmail.com>
2021-08-14 00:12:08 +0200

    Disable GC during startup

--- home/.emacs.d/early-init.el
+++ home/.emacs.d/early-init.el
@@ -1,1 +1,13 @@
+;; -*- lexical-binding: t; -*-
+
 (setq package-enable-at-startup nil)
+
+;; Temporarily disable the GC during initialization
+(defun ml-restore-gc-threshold ()
+  "Reset the GC threshold to a reasonable value."
+  (setq gc-cons-threshold (* 16 1024 1024)))
+(setq gc-cons-threshold most-positive-fixnum)
+(add-hook 'emacs-startup-hook #'ml-restore-gc-threshold)
+
+;; Disable splash screen
+(setq inhibit-startup-message t)
