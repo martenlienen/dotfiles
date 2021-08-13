b2f5f78a3ecc6b7b9961d4d6fbdd7adafdbf3276
Marten Lienen <marten.lienen@gmail.com>
2021-08-14 00:20:05 +0200

    Disable file-name handling during startup

--- home/.emacs.d/early-init.el
+++ home/.emacs.d/early-init.el
@@ -9,5 +9,14 @@
 (setq gc-cons-threshold most-positive-fixnum)
 (add-hook 'emacs-startup-hook #'ml-restore-gc-threshold)
 
+;; Temporarily disable special file name handling to improve library loading speed
+(defvar ml-default-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+(defun ml-restore-file-name-handler-alist ()
+  "Restore the file-name-handler alist."
+  (setq file-name-handler-alist ml-default-file-name-handler-alist))
+
+(add-hook 'emacs-startup-hook #'ml-restore-file-name-handler-alist)
+
 ;; Disable splash screen
 (setq inhibit-startup-message t)
