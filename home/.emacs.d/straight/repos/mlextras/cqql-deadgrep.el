61a11f5537d2335b5861c8dfc5be8fa1b3abcb28
Marten Lienen <marten.lienen@gmail.com>
2021-09-03 10:38:33 +0200

    Bind deadgrep directory and file types to keys

--- home/.emacs.d/straight/repos/mlextras/cqql-deadgrep.el
+++ home/.emacs.d/straight/repos/mlextras/cqql-deadgrep.el
@@ -10,6 +10,32 @@
   ;; Ignores the button so we can just call it
   (deadgrep--search-term nil))
 
+(defun cqql-deadgrep-directory ()
+  "Change the directory in deadgrep."
+  (interactive)
+  ;; Ignores the button so we can just call it
+  (deadgrep--directory nil))
+
+(defun cqql-deadgrep-file-type (type)
+  "Set the file type to TYPE."
+  (let ((button (make-button 0 0 :type 'deadgrep-file-type 'file-type type)))
+    (deadgrep--file-type button)))
+
+(defun cqql-deadgrep-file-type-all ()
+  "Search all file types in deadgrep."
+  (interactive)
+  (cqql-deadgrep-file-type 'all))
+
+(defun cqql-deadgrep-file-type-type ()
+  "Search certain file types in deadgrep."
+  (interactive)
+  (cqql-deadgrep-file-type 'type))
+
+(defun cqql-deadgrep-file-type-glob ()
+  "Select file types by glob in deadgrep."
+  (interactive)
+  (cqql-deadgrep-file-type 'glob))
+
 (defun cqql-deadgrep-here ()
   "Run a deadgrep search in the current buffer's directory."
   (interactive)
