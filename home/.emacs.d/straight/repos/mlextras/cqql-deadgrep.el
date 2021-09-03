8bceb6ca1f095f91c941d99cd49ff11bf26dfd6f
Marten Lienen <marten.lienen@gmail.com>
2021-09-03 10:10:56 +0200

    Deadgrep in current directory

--- home/.emacs.d/straight/repos/mlextras/cqql-deadgrep.el
+++ home/.emacs.d/straight/repos/mlextras/cqql-deadgrep.el
@@ -10,5 +10,12 @@
   ;; Ignores the button so we can just call it
   (deadgrep--search-term nil))
 
+(defun cqql-deadgrep-here ()
+  "Run a deadgrep search in the current buffer's directory."
+  (interactive)
+  (let* ((root default-directory)
+         (deadgrep-project-root-function (lambda () root)))
+    (call-interactively #'deadgrep)))
+
 (provide 'cqql-deadgrep)
 ;;; cqql-deadgrep.el ends here
