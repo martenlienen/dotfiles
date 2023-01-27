3808613fa1299a942a1ba2bc4d668455f44c6b3c
Marten Lienen <marten.lienen@gmail.com>
2023-01-27 17:13:10 +0100

    Install project.nvim

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -61,6 +61,13 @@ require("packer").startup(function(use)
   -- Fuzzy Finder (files, lsp, etc)
   use { "nvim-telescope/telescope.nvim", branch = "0.1.x", requires = { "nvim-lua/plenary.nvim" } }
 
+  use {
+    "ahmedkhalf/project.nvim",
+    config = function()
+      require("project_nvim").setup { }
+    end
+  }
+
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
   use { "nvim-telescope/telescope-fzf-native.nvim", run = "make", cond = vim.fn.executable "make" == 1 }
 
@@ -199,6 +206,7 @@ require("telescope").setup {
     },
   },
 }
+require("telescope").load_extension("projects")
 
 -- Enable telescope fzf native, if installed
 pcall(require("telescope").load_extension, "fzf")
