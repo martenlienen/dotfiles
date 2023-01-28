619478007e91e4eac493b0c4757518dcaefdb0f2
Marten Lienen <marten.lienen@gmail.com>
2023-01-28 14:43:15 +0100

    Configure lsp bindings for pylsp

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -371,6 +371,10 @@ require("neodev").setup()
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
+require("lspconfig").pylsp.setup{
+  on_attach = on_attach
+}
+
 -- Setup mason so it can manage external tooling
 require("mason").setup()
 
