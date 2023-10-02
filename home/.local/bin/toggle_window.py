8457c4e624afd11a63ac43a6ffc08c412101c351
Marten Lienen <marten.lienen@gmail.com>
2023-10-02 08:09:19 +0200

    Move floating windows to front if they are on the active workspace

--- home/.local/bin/toggle_window.py
+++ home/.local/bin/toggle_window.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import json
-from pathlib import Path
 import shutil
 import subprocess
 from argparse import ArgumentParser
@@ -22,18 +21,19 @@ def i3_layout_tree():
 class WorkspaceException(Exception):
     """Yes, this is abused for a non-local return."""
 
-    def __init__(self, workspace):
+    def __init__(self, workspace, node):
         self.workspace = workspace
+        self.node = node
 
 
-def find_program_workspace(window_class):
+def find_program_workspace_and_node(window_class):
     def search(node, workspace=None):
         if node.get("type") == "workspace":
             workspace = node
 
         class_name = node.get("window_properties", {}).get("class")
         if class_name is not None and class_name.lower() == window_class.lower():
-            raise WorkspaceException(workspace)
+            raise WorkspaceException(workspace, node)
         else:
             if "nodes" in node:
                 for child in node["nodes"]:
@@ -48,35 +48,17 @@ def find_program_workspace(window_class):
 
         raise Exception(f"{window_class} window not found in the layout tree")
     except WorkspaceException as e:
-        return e.workspace
+        return e.workspace, e.node
 
 
-def program_runs(program):
-    # We match against the process name, so that we don't match toggle_window itself.
-    # However, the process name is at most 16 null-terminated bytes [1], so we only
-    # search for the first 15 characters.
-    #
-    # [1] https://stackoverflow.com/questions/23534263/what-is-the-maximum-allowed-limit-on-the-length-of-a-process-name
-    pgrep = subprocess.run(["pgrep", program[:15]], stdout=subprocess.DEVNULL)
-    return pgrep.returncode == 0
-
-
-def start_program(program, args: list[str]):
-    path = shutil.which(program)
-    subprocess.Popen([path] + args, start_new_session=True)
-
-
-def program_is_visible(workspace, window_class):
+def workspace_contains_focus(workspace):
     class FoundIt(Exception):
         pass
 
-    # Check if the program's workspace contains the focused window. This could be not
-    # the case if for example the focused window is on a workspace on a different screen
-    # that is displayed concurrently.
+    # Check if the workspace contains a focused window
     def search(node):
-        class_ = node.get("window_properties", {}).get("instance", "")
         is_focused = node.get("focused", False)
-        if is_focused and class_.lower() == window_class.lower():
+        if is_focused:
             raise FoundIt()
 
         if "nodes" in node:
@@ -92,6 +74,21 @@ def program_is_visible(workspace, window_class):
         return False
     except FoundIt:
         return True
+
+
+def program_runs(program):
+    # We match against the process name, so that we don't match toggle_window itself.
+    # However, the process name is at most 16 null-terminated bytes [1], so we only
+    # search for the first 15 characters.
+    #
+    # [1] https://stackoverflow.com/questions/23534263/what-is-the-maximum-allowed-limit-on-the-length-of-a-process-name
+    pgrep = subprocess.run(["pgrep", program[:15]], stdout=subprocess.DEVNULL)
+    return pgrep.returncode == 0
+
+
+def start_program(program, args: list[str]):
+    path = shutil.which(program)
+    subprocess.Popen([path] + args, start_new_session=True)
 
 
 def program_is_on_scratchpad(workspace):
@@ -130,10 +127,16 @@ def main():
         window_class = program.title()
 
     if program_runs(program):
-        workspace = find_program_workspace(window_class)
+        workspace, node = find_program_workspace_and_node(window_class)
 
-        if program_is_visible(workspace, window_class):
-            hide_program(window_class)
+        if workspace_contains_focus(workspace):
+            if node["focused"]:
+                hide_program(window_class)
+            else:
+                # The program is visible on the active workspace but not focused, i.e.
+                # usually behind another window. We toggle it to raise it to the front.
+                hide_program(window_class)
+                show_program(window_class, width, height)
         elif program_is_on_scratchpad(workspace):
             show_program(window_class, width, height)
         else:
